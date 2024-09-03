2d4b4dc21cf93b984255921c86706acd214bdd09
lizard0126 <825456471@qq.com>
2024-09-03 17:28:02 +0800

    测试

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,97 @@
-import { Context, Schema } from 'koishi'
+import { Context, segment } from 'koishi'
+import Schema from 'schemastery'
 
-export const name = 'javbus-lizard'
 
-export interface Config {}
+export const name = 'javbus'
 
-export const Config: Schema<Config> = Schema.object({})
+export const usage = `
 
-export function apply(ctx: Context) {
-  // write your plugin here
+本插件自用，用于查询番号返回磁力链接与封面预览
+
+命令前缀jav XXXX-1234，api需要自行部署，参考https://github.com/ovnrain/javbus-api
+
+经测试2024/9/3可用
+
+该插件请低调使用, 请勿配置于QQ或者是其他国内APP平台, 带来的后果请自行承担
+`
+
+export interface Config {
+  apiPrefix: string;
+  allowDownloadLink: boolean;
+  allowPreviewCover: boolean;
+}
+
+export const Config = Schema.object({
+  apiPrefix: Schema.string()
+    .default('')
+    .required()
+    .description('填写api,形如https://aaa.bbb.ccc'),
+  allowDownloadLink: Schema.boolean()
+    .default(false)
+    .description('是否返回磁力链接'),
+})
+
+export const movieDetailApi = '/api/movies/';
+export const magnetDetailApi = '/api/magnets/';
+
+export function apply(ctx: Context, config: Config) {
+  interface MovieDetail {
+    gid?: number;
+    uc?: string;
+    magnets: string;
+    title?: string;
+    date?: string;
+    stars?: Array<{ name: string }>;
+  }
+  async function fetchMovieDetail(number: string): Promise<MovieDetail> {
+    const movieUrl = config.apiPrefix + movieDetailApi + number;
+    let result = {
+      magnets: "",
+    };
+
+    const movieData = await ctx.http.get(movieUrl);
+    const { title, id, gid, date, uc, stars } = movieData;
+    result = { ...result, ...{ title, id, gid, date, uc, stars } };
+
+    if (config.allowDownloadLink) {
+      const magnetsUrl =
+        config.apiPrefix + magnetDetailApi + `${id}?gid=${gid}&uc=${uc}`;
+      console.log("Fetching details from:", magnetsUrl);
+
+      const magnetsList = await ctx.http.get(magnetsUrl);
+
+      if (magnetsList.length > 0) {
+        result.magnets = `磁力: ${magnetsList[0].size},${magnetsList[0].shareDate}\n${magnetsList[0].link}`;
+
+        if (magnetsList.length > 1) {
+          result.magnets += `\n磁力2: ${
+            magnetsList[magnetsList.length - 1].size
+          },${magnetsList[magnetsList.length - 1].shareDate}\n${
+            magnetsList[magnetsList.length - 1].link
+          }`;
+        }
+      }
+    }
+    return result;
+  }
+
+  ctx.command('jav <number:text>', '查找javbus番号')
+    .action(async ({session}, number) => {
+      try {
+        if (!number) return '请提供番号!'
+        const result = await fetchMovieDetail(number)
+        const { title, magnets, date, stars, } = result;
+        const starsArray = stars.map(star => star.name);
+        const starsname = starsArray.length > 1 ? starsArray.join(', ') : starsArray[0];
+
+        if (!config.allowDownloadLink) {
+          await session.sendQueued(`标题: ${title}\n发行日期: ${date}\n女优: ${starsname}`)
+        } else {
+          await session.sendQueued(`标题: ${title}\n发行日期: ${date}\n女优: ${starsname}\n磁力: ${magnets}`)
+        }
+      } catch(err) {
+        console.log(err);
+        return `发生错误!请检查指令jav后是否添加空格，番号是否用-连接;  ${err}`;
+      }
+    });
 }
