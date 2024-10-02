43979f80b67feb749111127f78d85fb9e2cbd45c
lizard0126 <825456471@qq.com>
2024-10-02 16:57:54 +0800

    新增关键词搜索

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,44 @@ export const name = 'javbus-lizard'
 
 export const usage = `
 
-自用插件，用于查询番号返回磁力链接、封面预览、内容预览截图
+# NSFW警告!!!
+## 用于查询番号返回磁力链接、封面预览、内容预览截图
 
-项目整合修改自koishi-plugin-javbus和koishi-plugin-magnet-preview
+项目整合修改自javbus和magnet-preview，api需要自行部署，参考项目[javbus-api](https://github.com/ovnrain/javbus-api)
 
-命令前缀jav XXXX-1234，api需要自行部署，参考项目https://github.com/ovnrain/javbus-api
+经测试2024/10/2可用。请低调使用, 请勿配置于QQ或者是其他国内APP平台, 带来的后果请自行承担
 
-经测试2024/9/30可用。请低调使用, 请勿配置于QQ或者是其他国内APP平台, 带来的后果请自行承担
+## 主要功能及示例调用：
+- 番号搜索：
+  - 用户可以通过番号搜索电影的详细信息，包括电影标题、发行日期、女优。根据插件配置，还可以返回磁力链接、封面和预览图。
+  
+  - 示例指令：jav SSIS-834
+  
+  - 返回结果：
+    - 标题: 完全引退 AV女優、最後の1日。三上悠亜ラストセックス
+    - 发行日期: 2023-08-11
+    - 女优: 三上悠亜
+- 关键词搜索:
+  - 用户可以通过关键词搜索与关键词相关的最多五部电影。
+  
+  - 示例指令：jkw 三上
+  
+  - 返回结果：
+    前五个与关键词相关的影片，包括标题、番号和封面。
+
+## 改进：
+- 添加了对女优信息的处理和显示，处理了 stars 数组，确保女优名字正确格式化显示。
+
+- 限制返回的电影数量为五个，处理了无结果的情况，返回相应提示。
+
+- handleMagnetLink 函数负责处理磁力链接的预览图，结合了 Puppeteer 库，能在支持的环境中抓取页面截图。
+## todo：
+- 获取最新上传的影片列表
+
+- 搜索女优信息
+
+- ……
+
 `
 
 export interface Config {
@@ -40,6 +71,7 @@ export const Config = Schema.object({
 
 export const movieDetailApi = '/api/movies/';
 export const magnetDetailApi = '/api/magnets/';
+export const searchMovieApi = '/api/movies/search?keyword=';
 
 export function apply(ctx: Context, config: Config) {
   interface MovieDetail {
@@ -52,15 +84,18 @@ export function apply(ctx: Context, config: Config) {
     img?: string;
   }
 
-  async function captureElementAsBuffer(element) { //捕获截图
+  //捕获截图
+  async function captureElementAsBuffer(element) {
     return await element.screenshot({ omitBackground: true });
   }
   
-  async function getThumbFromMagnet(ctx, magnetLink) { //获取缩略图
+  //获取缩略图
+  async function getThumbFromMagnet(ctx, magnetLink) { 
     const hash = magnetLink.split(':')[3].split('&')[0];
     const url = `https://beta.magnet.pics/m/${hash}`;
     const customUA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36';
-    const page = await ctx.puppeteer.page(); //用pptr加载页面
+    //用pptr加载页面
+    const page = await ctx.puppeteer.page(); 
     await page.setUserAgent(customUA);
     await page.goto(url, { waitUntil: 'networkidle0' });
     const thumbContainer = await page.$('.thumb-container');
@@ -73,7 +108,8 @@ export function apply(ctx: Context, config: Config) {
     return imageBuffer;
   }
   
-  async function handleMagnetLink(ctx, session, url) { //处理磁链
+  //处理磁链
+  async function handleMagnetLink(ctx, session, url) { 
     const magnetLinkRegex = /magnet:\?xt=urn:btih:[a-zA-Z0-9]*/gi;
     if (!magnetLinkRegex.test(url)) {
       return '无效的磁力链接格式！';
@@ -118,6 +154,34 @@ export function apply(ctx: Context, config: Config) {
     return result;
   }
 
+  //根据关键词搜索电影
+  async function fetchMoviesByKeyword(keyword: string) { 
+    const searchUrl = config.apiPrefix + searchMovieApi + encodeURIComponent(keyword);
+    let result = [];
+
+    const searchData = await ctx.http.get(searchUrl);
+
+    if (!searchData || !Array.isArray(searchData.movies) || searchData.movies.length === 0) {
+    return '未找到任何匹配的电影！';
+    }
+
+    const limitedMovies = searchData.movies.slice(0, 5);
+
+    result = limitedMovies.map((movie) => {
+      const { title, id, img } = movie;
+
+      let message = `标题: ${title}\n番号: ${id}`;
+
+      if (config.allowPreviewCover) {
+        message += `\n封面: ${segment.image(img)}`;
+      }
+      return message;
+      
+    });
+
+    return result.join('\n\n');
+  }
+
   ctx.command('jav <number:text>', '查找javbus番号')
     .action(async ({session}, number) => {
       try {
@@ -155,4 +219,18 @@ export function apply(ctx: Context, config: Config) {
         return `发生错误!请检查网络连接、指令jav后是否添加空格、番号是否用-连接;  ${err}`;
       }
     });
+
+  ctx.command('jkw <keyword:text>', '通过关键词搜索')
+    .action(async ({ session }, keyword) => {
+      try {
+        if (!keyword) return '请提供关键词!';
+        const result = await fetchMoviesByKeyword(keyword);
+        return result;
+      
+      } catch(err) {
+        console.log(err);
+        return `发生错误!请检查网络连接、指令jkw后是否添加空格;  ${err}`;
+      }
+    });
+
 }
