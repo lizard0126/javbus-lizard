5a34b471851df547ae7a8c2d52ecadf2eb25a6a3
lizard0126 <825456471@qq.com>
2024-10-11 11:26:11 +0800

    新增最新影片获取功能

--- src/index.ts
+++ src/index.ts
@@ -11,35 +11,24 @@ export const usage = `
 
 项目整合修改自javbus和magnet-preview，api需要自行部署，参考项目[javbus-api](https://github.com/ovnrain/javbus-api)
 
-经测试2024/10/2可用。请低调使用, 请勿配置于QQ或者是其他国内APP平台, 带来的后果请自行承担
+经测试2024/10/11可用。请低调使用, 请勿配置于QQ或者是其他国内APP平台, 带来的后果请自行承担
 
 ## 主要功能及示例调用：
-- 番号搜索：
-  - 用户可以通过番号搜索电影的详细信息，包括电影标题、发行日期、女优。根据插件配置，还可以返回磁力链接、封面和预览图。
-  
-  - 示例指令：jav SSIS-834
-  
-  - 返回结果：
-    - 标题: 完全引退 AV女優、最後の1日。三上悠亜ラストセックス
-    - 发行日期: 2023-08-11
-    - 女优: 三上悠亜
-- 关键词搜索:
-  - 用户可以通过关键词搜索与关键词相关的最多五部电影。
-  
-  - 示例指令：jkw 三上
-  
-  - 返回结果：
-    前五个与关键词相关的影片，包括标题、番号和封面。
+- 番号搜索  示例指令：jav SSIS-834
+  - 通过番号搜索影片的详细信息，包括影片标题、发行日期、女优。根据插件配置，还可以返回磁力链接、封面和预览图。
+
+- 关键词搜索  示例指令：jkw 三上
+  - 通过关键词搜索与关键词相关的最多五部影片。
+
+- 最新影片  示例指令：jew/jew无码
+  - 获取最新上传的最多五部影片。
 
 ## 改进：
-- 添加了对女优信息的处理和显示，处理了 stars 数组，确保女优名字正确格式化显示。
+- 新增了最新上传的影片列表获取，分为有码和无码
 
-- 限制返回的电影数量为五个，处理了无结果的情况，返回相应提示。
+- 完善了番号搜索的返回，将封面与影片信息分开发送
 
-- handleMagnetLink 函数负责处理磁力链接的预览图，结合了 Puppeteer 库，能在支持的环境中抓取页面截图。
 ## todo：
-- 获取最新上传的影片列表
-
 - 搜索女优信息
 
 - ……
@@ -72,6 +61,7 @@ export const Config = Schema.object({
 export const movieDetailApi = '/api/movies/';
 export const magnetDetailApi = '/api/magnets/';
 export const searchMovieApi = '/api/movies/search?keyword=';
+export const uncensoredMovieApi = '/api/movies?type=uncensored';
 
 export function apply(ctx: Context, config: Config) {
   interface MovieDetail {
@@ -84,17 +74,14 @@ export function apply(ctx: Context, config: Config) {
     img?: string;
   }
 
-  //捕获截图
   async function captureElementAsBuffer(element) {
     return await element.screenshot({ omitBackground: true });
   }
   
-  //获取缩略图
   async function getThumbFromMagnet(ctx, magnetLink) { 
     const hash = magnetLink.split(':')[3].split('&')[0];
     const url = `https://beta.magnet.pics/m/${hash}`;
     const customUA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36';
-    //用pptr加载页面
     const page = await ctx.puppeteer.page(); 
     await page.setUserAgent(customUA);
     await page.goto(url, { waitUntil: 'networkidle0' });
@@ -108,7 +95,6 @@ export function apply(ctx: Context, config: Config) {
     return imageBuffer;
   }
   
-  //处理磁链
   async function handleMagnetLink(ctx, session, url) { 
     const magnetLinkRegex = /magnet:\?xt=urn:btih:[a-zA-Z0-9]*/gi;
     if (!magnetLinkRegex.test(url)) {
@@ -144,42 +130,10 @@ export function apply(ctx: Context, config: Config) {
       const magnetsList = await ctx.http.get(magnetsUrl);
 
       if (magnetsList.length > 0) {
-        result.magnets = `磁力: 
-          ${magnetsList[0].size},
-          ${magnetsList[0].shareDate}
-          \n${magnetsList[0].link}`;
-
+        result.magnets = `大小：${magnetsList[0].size}\n${magnetsList[0].link}`;
       }
     }
     return result;
-  }
-
-  //根据关键词搜索电影
-  async function fetchMoviesByKeyword(keyword: string) { 
-    const searchUrl = config.apiPrefix + searchMovieApi + encodeURIComponent(keyword);
-    let result = [];
-
-    const searchData = await ctx.http.get(searchUrl);
-
-    if (!searchData || !Array.isArray(searchData.movies) || searchData.movies.length === 0) {
-    return '未找到任何匹配的电影！';
-    }
-
-    const limitedMovies = searchData.movies.slice(0, 5);
-
-    result = limitedMovies.map((movie) => {
-      const { title, id, img } = movie;
-
-      let message = `标题: ${title}\n番号: ${id}`;
-
-      if (config.allowPreviewCover) {
-        message += `\n封面: ${segment.image(img)}`;
-      }
-      return message;
-      
-    });
-
-    return result.join('\n\n');
   }
 
   ctx.command('jav <number:text>', '查找javbus番号')
@@ -197,11 +151,11 @@ export function apply(ctx: Context, config: Config) {
           message += `\n磁力: ${magnets}`;
         }
 
-        if (config.allowPreviewCover && img) {
-          message += `\n封面: ${segment.image(img)}`;
-        }
-
         await session.sendQueued(message);
+
+        if (config.allowPreviewCover && img) {
+          await session.sendQueued(segment.image(img));
+        }
 
         if (config.allowPreviewMovie && result.magnets) {
           const magnetLink = result.magnets.split('\n').pop();
@@ -220,6 +174,34 @@ export function apply(ctx: Context, config: Config) {
       }
     });
 
+  //根据关键词搜索av
+  async function fetchMoviesByKeyword(keyword: string) { 
+    const searchUrl = config.apiPrefix + searchMovieApi + encodeURIComponent(keyword);
+    let result = [];
+
+    const searchData = await ctx.http.get(searchUrl);
+
+    if (!searchData || !Array.isArray(searchData.movies) || searchData.movies.length === 0) {
+    return '未找到任何匹配的电影！';
+    }
+
+    const limitedMovies = searchData.movies.slice(0, 5);
+
+    result = limitedMovies.map((movie) => {
+      const { title, id, img, date } = movie;
+
+      let message = `标题: ${title}\n发行日期: ${date}\n番号: ${id}`;
+
+      if (config.allowPreviewCover) {
+        message += `\n封面: ${segment.image(img)}`;
+      }
+      return message;
+      
+    });
+
+    return result.join('\n\n');
+  }
+
   ctx.command('jkw <keyword:text>', '通过关键词搜索')
     .action(async ({ session }, keyword) => {
       try {
@@ -233,4 +215,77 @@ export function apply(ctx: Context, config: Config) {
       }
     });
 
+  // 获取最新有码av列表
+async function fetchMovies() {
+  const latestMoviesUrl = config.apiPrefix + movieDetailApi;
+  const movieList = await ctx.http.get(latestMoviesUrl);
+  let result = [];
+
+  if (!movieList || !Array.isArray(movieList.movies) || movieList.movies.length === 0) {
+    return '未找到任何影片！';
+  }
+
+  const limitedMovies = movieList.movies.slice(0, 5);
+
+  result = limitedMovies.map((movie) => {
+    const { title, id, img, date, tags } = movie;
+
+    let message = `标题: ${title}\n番号: ${id}\n发行日期: ${date}\n标签: ${tags.join(', ')}`;
+
+    if (config.allowPreviewCover) {
+      message += `\n封面: ${segment.image(img)}`;
+    }
+    return message;
+  });
+
+  return result.join('\n\n');
+}
+
+ctx.command('jew', '获取最新的影片')
+  .action(async ({ session }) => {
+    try {
+      const result = await fetchMovies();
+      return result;
+    } catch (err) {
+      console.log(err);
+      return `获取影片失败！请检查网络连接: ${err}`;
+    }
+  });
+
+  // 获取最新无码av列表
+async function fetchUncensoredMovies() {
+  const uncensoredMoviesUrl = config.apiPrefix + uncensoredMovieApi;
+  const uncensored = await ctx.http.get(uncensoredMoviesUrl);
+  let result = [];
+
+  if (!uncensored || !Array.isArray(uncensored.movies) || uncensored.movies.length === 0) {
+    return '未找到任何影片！';
+  }
+
+  const limitedMovies = uncensored.movies.slice(0, 5);
+
+  result = limitedMovies.map((movie) => {
+    const { title, id, img, date } = movie;
+
+    let message = `标题: ${title}\n番号: ${id}\n发行日期: ${date}`;
+
+    if (config.allowPreviewCover) {
+      message += `\n封面: ${segment.image(img)}`;
+    }
+    return message;
+  });
+
+  return result.join('\n\n');
+}
+
+ctx.command('jew无码', '获取最新的无码影片')
+  .action(async ({ session }) => {
+    try {
+      const result = await fetchUncensoredMovies();
+      return result;
+    } catch (err) {
+      console.log(err);
+      return `获取影片失败！请检查网络连接: ${err}`;
+    }
+  });
 }
